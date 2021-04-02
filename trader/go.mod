2a51543bb4689f9b89e8ce4cb57ca57d88858b7d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-04-03 00:19:08 +0900

    update: MySQL on dockerとの接続チェック

--- trader/go.mod
+++ trader/go.mod
@@ -1,3 +1,5 @@
 module github.com/Fukkatsuso/cryptocurrency-trading-bot/trader
 
 go 1.14
+
+require github.com/go-sql-driver/mysql v1.6.0
--- trader/main.go
+++ trader/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
+var timeFormat = "2006-01-02 15:04:05"
+
+func init() {
+	var err error
+	db, err = sql.Open("mysql", "trading_app:password@tcp(db:3306)/trading_db")
+	if err != nil {
+		panic(err.Error())
+	}
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
+	// DBに接続してeth_candlesテーブルの情報(レコードのcount?)を取得，何か値を表示させる
+	cmd := `SELECT COUNT(time) FROM eth_candles`
+	row := db.QueryRow(cmd)
+	var count int
+	if err := row.Scan(&count); err != nil {
+		fmt.Fprintf(w, err.Error())
+		return
+	}
+
 	name := os.Getenv("NAME")
 	if name == "" {
 		name = "World"
 	}
-	fmt.Fprintf(w, "Hello %s! (trader)\n", name)
+	fmt.Fprintf(w, "Hello %s! (trader)\ncount: %d\n", name, count)
+}
+
+func saveCandle(tm time.Time) {
+	cmd := `INSERT INTO eth_candles (time, open, close, high, low, volume) VALUES (?, 1.0, 2.0, 3.0, 4.0, 5.0)`
+	_, err := db.Exec(cmd, tm.Format(timeFormat))
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
 
 func main() {
@@ -25,6 +58,20 @@ func main() {
 		port = "8080"
 		log.Printf("defaulting to port %s", port)
 	}
+
+	// 3秒おきにDBのデータを更新(適当な値を挿入する)
+	// あくまでもDB接続のテストであり，定期実行はGoでは扱わないため，動けばOK
+	go func() {
+		t := time.NewTicker(3 * time.Second)
+		defer t.Stop()
+		for {
+			select {
+			case now := <-t.C:
+				fmt.Println(now.Format(timeFormat))
+				saveCandle(now)
+			}
+		}
+	}()
 
 	// Start HTTP server.
 	log.Printf("listening on port %s", port)
