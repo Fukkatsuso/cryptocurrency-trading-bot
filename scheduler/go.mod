8b841ac1cf2174358fb31ce30fc5cdbd386b1453
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-04-20 23:25:30 +0900

    add: scheduler(1分おきにticker取得させる)

---
+++ scheduler/go.mod
@@ -0,0 +1,5 @@
+module github.com/Fukkatsuso/cryptocurrency-trading-bot/scheduler
+
+go 1.14
+
+require github.com/robfig/cron/v3 v3.0.1
---
+++ scheduler/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/robfig/cron/v3"
+)
+
+func traderFetchTicker() {
+	url := "http://trading_trader:8080/fetch-ticker"
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	log.Println("[cron]", resp.StatusCode, resp.Request.URL)
+}
+
+func main() {
+	c := cron.New()
+	c.AddFunc("*/1 * * * *", traderFetchTicker)
+	c.Start()
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
