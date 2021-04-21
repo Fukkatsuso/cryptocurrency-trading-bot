a1850d35cc260d32f3cfb798ed6bfa824ab1f6a7
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-04-22 00:31:20 +0900

    fix: local time

--- trader/router/logger.go
+++ trader/router/logger.go
@@ -8,7 +8,7 @@ import (
 
 func logger(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		now := time.Now().Format("2006-01-02 15:04:05")
+		now := time.Now().Local().Format("2006-01-02 15:04:05")
 		method := r.Method
 		path := r.URL.Path
 		fmt.Printf("%s [%s] %s", now, method, path)
