05d33168d55a425e4b7fdad8ec16a83bf40a94f2
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-04-30 00:10:18 +0900

    add: trader/model/data_frame.go

---
+++ trader/model/data_frame.go
@@ -0,0 +1,55 @@
+package model
+
+import "time"
+
+type DataFrame struct {
+	Candles []Candle
+}
+
+func (df *DataFrame) Times() []time.Time {
+	s := make([]time.Time, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Time
+	}
+	return s
+}
+
+func (df *DataFrame) Opens() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Open
+	}
+	return s
+}
+
+func (df *DataFrame) Closes() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Close
+	}
+	return s
+}
+
+func (df *DataFrame) Highs() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.High
+	}
+	return s
+}
+
+func (df *DataFrame) Lows() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Low
+	}
+	return s
+}
+
+func (df *DataFrame) Volumes() []float64 {
+	s := make([]float64, len(df.Candles))
+	for i, candle := range df.Candles {
+		s[i] = candle.Volume
+	}
+	return s
+}
