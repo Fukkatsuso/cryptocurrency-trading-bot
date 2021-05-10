42337b93413e875d7e473050044ee119c4b902a1
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-05-10 23:14:51 +0900

    update: /api/candleから取得してそのまま表示

--- dashboard/view/js/index.js
+++ dashboard/view/js/index.js
@@ -4,7 +4,37 @@ new Vue({
   vuetify: new Vuetify(),
   data() {
     return {
-      message: "Hello World"
+      candle: null,
     }
+  },
+  methods: {
+    async getCandle() {
+      let params = {
+        "limit": 10,
+      }
+      return await axios.get('/api/candle', {
+        params: params,
+      }).then(res => {
+        return res.data
+      }).catch(err => {
+        console.log(err)
+        return null
+      })
+    },
+    drawChart() {
+      console.log("drawChart")
+    },
+    async update() {
+      // キャンドルデータとインディケータを取得
+      this.candle = await this.getCandle()
+      if (!this.candle) {
+        return
+      }
+      // this.candleを使ってグラフ描画
+      this.drawChart()
+    }
+  },
+  mounted: async function() {
+    this.update()
   },
 })
