18621d399a209cc15b2e9bdd5bac4dbd4cccbcfa
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-05-16 20:17:38 +0900

    update: /api/candle クエリパラメータをもとに指標を付けてレスポンス

--- dashboard/controller/api_candle.go
+++ dashboard/controller/api_candle.go
@@ -10,12 +10,22 @@ import (
 	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/model"
 )
 
+// URLクエリパラメータから非負整数を取り出す
+// エラーが生じたらデフォルトの値defを返す
+func getQueryUintDefault(r *http.Request, query string, def int) int {
+	strVal := r.URL.Query().Get(query)
+	intVal, err := strconv.Atoi(strVal)
+	if strVal == "" || err != nil || intVal < 0 {
+		intVal = def
+	}
+	return intVal
+}
+
 // candleデータとテクニカル指標をjsonで返す
 func APICandleHandler(w http.ResponseWriter, r *http.Request) {
 	// limit: candle最大数．[0, 1000]の範囲に限定
-	strLimit := r.URL.Query().Get("limit")
-	limit, err := strconv.Atoi(strLimit)
-	if strLimit == "" || err != nil || limit < 0 || limit > 1000 {
+	limit := getQueryUintDefault(r, "limit", 1000)
+	if limit > 1000 {
 		limit = 1000
 	}
 
@@ -25,16 +35,57 @@ func APICandleHandler(w http.ResponseWriter, r *http.Request) {
 		Candles: candles,
 	}
 
-	df.AddSMA(3)
-	df.AddSMA(7)
-	df.AddSMA(14)
-	df.AddEMA(3)
-	df.AddEMA(7)
-	df.AddEMA(14)
-	df.AddBBands(3, float64(2))
-	df.AddIchimoku()
-	df.AddRSI(7)
-	df.AddMACD(3, 7, 5)
+	// 移動平均線
+	sma := r.URL.Query().Get("sma")
+	if sma == "true" {
+		period1 := getQueryUintDefault(r, "smaPeriod1", 7)
+		period2 := getQueryUintDefault(r, "smaPeriod2", 14)
+		period3 := getQueryUintDefault(r, "smaPeriod3", 50)
+		df.AddSMA(period1)
+		df.AddSMA(period2)
+		df.AddSMA(period3)
+	}
+
+	// 指数平滑移動平均線
+	ema := r.URL.Query().Get("ema")
+	if ema == "true" {
+		period1 := getQueryUintDefault(r, "emaPeriod1", 7)
+		period2 := getQueryUintDefault(r, "emaPeriod2", 14)
+		period3 := getQueryUintDefault(r, "emaPeriod3", 50)
+		df.AddEMA(period1)
+		df.AddEMA(period2)
+		df.AddEMA(period3)
+	}
+
+	// ボリンジャーバンド
+	bbands := r.URL.Query().Get("bbands")
+	if bbands == "true" {
+		n := getQueryUintDefault(r, "bbandsN", 20)
+		k := getQueryUintDefault(r, "bbandsK", 2)
+		df.AddBBands(n, float64(k))
+	}
+
+	// 一目均衡表
+	ichimoku := r.URL.Query().Get("ichimoku")
+	if ichimoku == "true" {
+		df.AddIchimoku()
+	}
+
+	// RSI(Relative Strength Index, 相対力指数)
+	rsi := r.URL.Query().Get("rsi")
+	if rsi == "true" {
+		period := getQueryUintDefault(r, "rsiPeriod", 14)
+		df.AddRSI(period)
+	}
+
+	// MACD(Moving Average Convergence/Divergence, 移動平均・収束拡散)
+	macd := r.URL.Query().Get("macd")
+	if macd == "true" {
+		period1 := getQueryUintDefault(r, "macdPeriod1", 12)
+		period2 := getQueryUintDefault(r, "macdPeriod2", 26)
+		period3 := getQueryUintDefault(r, "macdPeriod3", 9)
+		df.AddMACD(period1, period2, period3)
+	}
 
 	js, err := json.Marshal(df)
 	if err != nil {
