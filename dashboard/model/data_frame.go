be4e26ec26b3851d4ca916ca98df6580372af59c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-05-27 00:55:39 +0900

    update: dashboard BackTest（売買シグナルは出さない）

--- dashboard/model/data_frame.go
+++ dashboard/model/data_frame.go
@@ -3,7 +3,6 @@ package model
 import (
 	"time"
 
-	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/config"
 	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/lib/trading"
 	"github.com/markcheno/go-talib"
 )
@@ -225,14 +224,76 @@ type TradeParams struct {
 }
 
 func (df *DataFrame) BackTest(params *TradeParams) {
+	if params == nil {
+		return
+	}
+
 	events := NewSignalEvents()
-	events.Signals = append(events.Signals, SignalEvent{
-		Time:        time.Now().In(config.LocalTime),
-		ProductCode: df.ProductCode,
-		Side:        "BUY",
-		Price:       1.0,
-		Size:        1.0,
-	})
+	for i := 1; i < len(df.Candles); i++ {
+		buyPoint, sellPoint := 0, 0
+
+		if params.EMAEnable && params.EMAPeriod1 <= i && params.EMAPeriod2 <= i {
+			emaValue1Prev, emaValue1 := df.EMAs[0].Values[i-1], df.EMAs[0].Values[i-1]
+			emaValue2Prev, emaValue2 := df.EMAs[1].Values[i-1], df.EMAs[1].Values[i-1]
+			if emaValue1Prev < emaValue2Prev && emaValue1 >= emaValue2 {
+				buyPoint++
+			}
+			if emaValue1Prev > emaValue2Prev && emaValue1 <= emaValue2 {
+				sellPoint++
+			}
+		}
+
+		if params.BBandsEnable && params.BBandsN <= i {
+			bbandsUpPrev, bbandsUp := df.BBands.Up[i-1], df.BBands.Up[i]
+			bbandsDownPrev, bbandsDown := df.BBands.Down[i-1], df.BBands.Down[i]
+			if bbandsDownPrev > df.Candles[i-1].Close && bbandsDown <= df.Candles[i].Close {
+				buyPoint++
+			}
+			if bbandsUpPrev < df.Candles[i-1].Close && bbandsUp >= df.Candles[i].Close {
+				sellPoint++
+			}
+		}
+
+		if params.IchimokuEnable {
+			tenkan := df.IchimokuCloud.Tenkan[i]
+			kijun := df.IchimokuCloud.Kijun[i]
+			senkouA := df.IchimokuCloud.SenkouA[i]
+			senkouB := df.IchimokuCloud.SenkouB[i]
+			chikouPrev, chikou := df.IchimokuCloud.Chikou[i-1], df.IchimokuCloud.Chikou[i]
+			if chikouPrev < df.Candles[i-1].High && chikou >= df.Candles[i].High &&
+				senkouA < df.Candles[i].Low && senkouB < df.Candles[i].Low &&
+				tenkan > kijun {
+				buyPoint++
+			}
+			if chikouPrev > df.Candles[i-1].Low && chikou <= df.Candles[i].Low &&
+				senkouA > df.Candles[i].High && senkouB > df.Candles[i].High &&
+				tenkan < kijun {
+				sellPoint++
+			}
+		}
+
+		if params.RSIEnable && df.RSI.Values[i-1] != 0 && df.RSI.Values[i-1] != 100 {
+			rsiPrev, rsi := df.RSI.Values[i-1], df.RSI.Values[i]
+			if rsiPrev < params.RSISellThread && rsi >= params.RSIBuyThread {
+				buyPoint++
+			}
+			if rsiPrev > params.RSISellThread && rsi <= params.RSISellThread {
+				sellPoint++
+			}
+		}
+
+		if params.MACDEnable {
+			macdPrev, macd := df.MACD.MACD[i-1], df.MACD.MACD[i]
+			signalPrev, signal := df.MACD.MACDSignal[i-1], df.MACD.MACDSignal[i]
+			if macd < 0 && signal < 0 && macdPrev < signalPrev && macd >= signal {
+				buyPoint++
+			}
+			if macd > 0 && signal > 0 && macdPrev > signalPrev && macd <= signal {
+				sellPoint++
+			}
+		}
+	}
 	df.BacktestEvents = events
+
 	df.BacktestEvents.EstimateProfit()
 }
