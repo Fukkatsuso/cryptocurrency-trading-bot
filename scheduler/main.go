7761a023d385e26660a05ce8ad2a2f9547c42cd5
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-06-22 22:52:24 +0900

    add: scheduler cronでtraderの取引実行APIを叩く

--- scheduler/main.go
+++ scheduler/main.go
@@ -23,9 +23,26 @@ func traderFetchTicker() {
 	log.Println("[cron]", resp.StatusCode, resp.Request.URL)
 }
 
+func traderTrade() {
+	url := "http://trading_trader:8080/trade"
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		return
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	log.Println("[cron]", resp.StatusCode, resp.Request.URL)
+}
+
 func main() {
 	c := cron.New()
 	c.AddFunc("*/5 * * * *", traderFetchTicker)
+	c.AddFunc("*/10 * * * *", traderTrade)
 	c.Start()
 
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {})
