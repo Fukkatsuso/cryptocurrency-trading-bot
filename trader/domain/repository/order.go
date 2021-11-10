c73e503d0555583ac7811deb958cc61e9e44695c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-11-10 17:47:43 +0900

    fix: Orderリポジトリのインタフェース

--- trader/domain/repository/order.go
+++ trader/domain/repository/order.go
@@ -4,5 +4,4 @@ import "github.com/Fukkatsuso/cryptocurrency-trading-bot/trader/domain/model"
 
 type OrderRepository interface {
 	Send(order model.Order) (*model.Order, error)
-	FetchById(productCode, orderId string) ([]model.Order, error)
 }
