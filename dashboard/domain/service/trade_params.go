5022cc11d13ddd2e1aeebcde9cf4ac66874e015c
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2021-12-11 18:54:27 +0900

    fix: EMAのパラメータ最適化プログラム

--- dashboard/domain/service/trade_params.go
+++ dashboard/domain/service/trade_params.go
@@ -43,8 +43,8 @@ func (ts *tradeParamsService) OptimizeEMA(df *model.DataFrame, fastPeriod, slowP
 	bestFastPeriod := fastPeriod
 	bestSlowPeriod := slowPeriod
 
-	for fastPeriod = 5; fastPeriod < 11; fastPeriod++ {
-		for slowPeriod = 12; slowPeriod < 20; slowPeriod++ {
+	for fastPeriod := 5; fastPeriod < 11; fastPeriod++ {
+		for slowPeriod := 12; slowPeriod < 20; slowPeriod++ {
 			signalEvents := ts.dataFrameService.BacktestEMA(df, fastPeriod, slowPeriod, size)
 			if signalEvents == nil {
 				continue
