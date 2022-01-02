06e95092186a15ebee60e2ee3dbce2b4c510e27d
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-01-02 14:10:41 +0900

    add: Userリポジトリ

---
+++ dashboard/infrastructure/persistence/user_test.go
@@ -0,0 +1,51 @@
+package persistence_test
+
+import (
+	"testing"
+
+	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/config"
+	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/domain/model"
+	"github.com/Fukkatsuso/cryptocurrency-trading-bot/dashboard/infrastructure/persistence"
+)
+
+func TestUser(t *testing.T) {
+	db := persistence.NewMySQLTransaction(config.DSN())
+	defer db.Rollback()
+
+	userRepository := persistence.NewUserRepository(db)
+
+	t.Run("save", func(t *testing.T) {
+		user := model.NewUser("test", "password", "")
+		err := userRepository.Save(user)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+	})
+
+	t.Run("find by user_id", func(t *testing.T) {
+		user, err := userRepository.FindByID("test")
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		if user.Password() != "password" {
+			t.Fatalf("%s != %s", user.Password(), "password")
+		}
+	})
+
+	t.Run("update password", func(t *testing.T) {
+		// "test"ユーザのパスワードを"qwerty"に更新
+		user := model.NewUser("test", "qwerty", "")
+		err := userRepository.Save(user)
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+
+		user, err = userRepository.FindByID("test")
+		if err != nil {
+			t.Fatal(err.Error())
+		}
+		if user.Password() != "qwerty" {
+			t.Fatalf("%s != %s", user.Password(), "qwerty")
+		}
+	})
+}
