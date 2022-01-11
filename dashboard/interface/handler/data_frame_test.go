477272f552e84f4e7032d5d23c21ae560b7d227a
Fukkatsuso <fukkatsuso.git+github@gmail.com>
2022-01-11 21:02:00 +0900

    fix: dasboard DataFrameハンドラのGetメソッドのテスト

--- dashboard/interface/handler/data_frame_test.go
+++ dashboard/interface/handler/data_frame_test.go
@@ -42,6 +42,7 @@ func TestDataFrameHandler(t *testing.T) {
 		}
 
 		query := req.URL.Query()
+		query.Add("size", "0.01")
 		query.Add("sma", "true")
 		query.Add("smaPeriod1", "7")
 		query.Add("smaPeriod2", "14")
@@ -56,10 +57,13 @@ func TestDataFrameHandler(t *testing.T) {
 		query.Add("ichimoku", "true")
 		query.Add("rsi", "true")
 		query.Add("rsiPeriod", "14")
+		query.Add("rsiBuyThread", "30")
+		query.Add("rsiSellThread", "70")
 		query.Add("macd", "true")
 		query.Add("macdPeriod1", "12")
 		query.Add("macdPeriod2", "26")
 		query.Add("macdPeriod3", "9")
+		query.Add("stopLimitPercent", "0.75")
 		query.Add("limit", "365")
 		req.URL.RawQuery = query.Encode()
 
